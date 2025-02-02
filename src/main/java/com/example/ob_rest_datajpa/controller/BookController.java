f73d02c47ab2016e150a68df0da2ba9a040fae74
SophyRJ <sofiarojasc5@gmail.com>
2025-02-02 20:53:50 +0100

    metodo POST obteniendo valores de peticiones

--- src/main/java/com/example/ob_rest_datajpa/controller/BookController.java
+++ src/main/java/com/example/ob_rest_datajpa/controller/BookController.java
@@ -2,10 +2,12 @@ package com.example.ob_rest_datajpa.controller;
 
 import com.example.ob_rest_datajpa.entities.Book;
 import com.example.ob_rest_datajpa.repository.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -31,9 +33,29 @@ public class BookController {
         return bookRepository.findAll();
     }
 
-    //Buscar un solo libro en bd segun id
-    //public Book findById(Long id){}
-    //Crear un nuevo libro en base de datos
+    //Buscar un solo libro en base de datos segun id
+    @GetMapping("/api/books/{id}")
+    public ResponseEntity<Book> findOneById(@PathVariable Long id){
+        Optional<Book> bookOpt= bookRepository.findById(id);
+        //Opcion 1
+        if(bookOpt.isPresent())
+            //Devuelve una respuesta OK con un valor ->el libro
+             return ResponseEntity.ok(bookOpt.get());
+          else
+              //Devuelve una respuesta NOTFOUND
+             return ResponseEntity.notFound().build();
+
+        //Opcion 2
+        //return bookOpt.orElse(null);
+        //return bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    //Crear un nuevo libro en base de datos -> que devuelva el libro creado
+    @PostMapping("/api/books")
+    public Book create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        //Guardar el libro recibido por parametro en la base de datos y devolvemos el nuevo libro
+        return bookRepository.save(book);
+    }
     //actualizar un libro existente en base de datos
     //Borrar un libro en bd
 }
